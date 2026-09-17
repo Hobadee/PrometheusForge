ebd586fa5e645d6da6e0adb255713572a02790c3
Eric Kincl <eric@kincl.net>
2026-09-17 03:41:39 -0700

    Damn freaking close to MVP!  LETS GOOOO!

--- Classes/Plugins/Task/Asana/AsanaCreateTaskDependency.ps1
+++ Classes/Plugins/Task/Asana/AsanaCreateTaskDependency.ps1
@@ -1,10 +1,16 @@
 class AsanaCreateTaskDependency : AsanaTaskPluginBase {
     <#
     .SYNOPSIS
-    Task plugin that marks one Asana task as dependent on another.
+    Task plugin that links Asana tasks as predecessors or successors of an anchor task.
 
     .DESCRIPTION
-    Boilerplate only - no Asana API calls are implemented yet.
+    Wraps both POST /tasks/{task_gid}/addDependencies and POST /tasks/{task_gid}/addDependents,
+    which are functionally identical aside from which side of the relationship taskGid sits on.
+    The 'relation' parameter picks the direction, using 'predecessor'/'successor' terminology
+    instead of Asana's 'dependency'/'dependent' (too easy to mix up) since it makes plain which
+    task must complete first:
+    - 'predecessor': relatedTaskGids must be completed before taskGid can start (addDependencies).
+    - 'successor': relatedTaskGids happen after taskGid is completed (addDependents).
     #>
 
     AsanaCreateTaskDependency() : base() {
@@ -28,31 +34,51 @@ class AsanaCreateTaskDependency : AsanaTaskPluginBase {
     [void] ValidateAsanaParameters([object]$params) {
         <#
         .SYNOPSIS
-        Validates parameters required to create an Asana task dependency.
+        Validates parameters required to link Asana tasks as predecessors/successors.
 
         .DESCRIPTION
         Expected parameters:
-        - taskGid: The gid of the task that has a dependency (the dependent task), as a string.
-        - dependsOnTaskGid: The gid of the task that must be completed first, as a string.
+        - taskGid: The gid of the anchor task, as a string.
+        - relatedTaskGids: An array of task gids to relate to the anchor task.
+        - relation: Either 'predecessor' (relatedTaskGids must complete before taskGid) or
+          'successor' (relatedTaskGids happen after taskGid).
         #>
-        if ($null -eq $params.taskGid) {
-            throw [System.ArgumentException]::new("Parameters must include a 'taskGid' value.", 'taskGid')
+        if ([string]::IsNullOrWhiteSpace([string]$params.taskGid)) {
+            throw [System.ArgumentException]::new("Parameters must include a non-empty 'taskGid' value.", 'taskGid')
         }
 
-        if ($null -eq $params.dependsOnTaskGid) {
-            throw [System.ArgumentException]::new("Parameters must include a 'dependsOnTaskGid' value.", 'dependsOnTaskGid')
+        if ($null -eq $params.relatedTaskGids -or $params.relatedTaskGids -is [string] -or $params.relatedTaskGids -isnot [System.Collections.IEnumerable]) {
+            throw [System.ArgumentException]::new("Parameters must include a 'relatedTaskGids' array.", 'relatedTaskGids')
+        }
+
+        $relatedTaskGids = @($params.relatedTaskGids)
+        if ($relatedTaskGids.Count -eq 0 -or @($relatedTaskGids | Where-Object { [string]::IsNullOrWhiteSpace([string]$_) }).Count -gt 0) {
+            throw [System.ArgumentException]::new("Parameter 'relatedTaskGids' must contain non-empty task gids.", 'relatedTaskGids')
+        }
+
+        $validRelations = @('predecessor', 'successor')
+        if ($validRelations -notcontains $params.relation) {
+            throw [System.ArgumentException]::new("Parameter 'relation' must be one of: $($validRelations -join ', ')", 'relation')
         }
     }
 
     [object] Execute() {
         <#
         .SYNOPSIS
-        Marks one Asana task as dependent on another.
+        Links the related tasks to the anchor task as predecessors or successors.
 
-        .NOTES
-        Not implemented - boilerplate only.
+        .DESCRIPTION
+        'predecessor' calls addDependencies with { dependencies: relatedTaskGids }; 'successor'
+        calls addDependents with { dependents: relatedTaskGids }. Same shape, opposite endpoint.
         #>
-        throw [System.NotImplementedException]::new("AsanaCreateTaskDependency.Execute() is not yet implemented")
+        $taskGid = [string]$this.parameters.taskGid
+        $relatedTaskGids = @($this.parameters.relatedTaskGids)
+
+        if ($this.parameters.relation -eq 'predecessor') {
+            return $this.InvokeAsanaApi('POST', "/tasks/$taskGid/addDependencies", @{ dependencies = $relatedTaskGids })
+        }
+
+        return $this.InvokeAsanaApi('POST', "/tasks/$taskGid/addDependents", @{ dependents = $relatedTaskGids })
     }
 }
 
